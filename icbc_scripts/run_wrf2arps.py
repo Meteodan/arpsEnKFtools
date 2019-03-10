17f378a9cfb23f8e9a44c194bef1447146b1ec7f
Dan Dawson <dawson29@purdue.edu>
2019-03-10 17:43:48 -0400

    Added wrf2arps input template and scripts

--- icbc_scripts/run_wrf2arps.py
+++ icbc_scripts/run_wrf2arps.py
@@ -6,13 +6,9 @@
 # files.
 # Based on an older script for arpsintrp from RLT
 
-import os
-import glob
-import sys
 import subprocess
-sys.path.append('/Users/dawson29/arpsEnKFtools/modules/')
-from editNamelist import editNamelistFile
-
+from datetime import datetime, timedelta
+from arpsenkftools.editNamelist import editNamelistFile
 
 ### USER SPECIFICATIONS ###
 # Absolute path to directory containing NEWS-e WRF history files
@@ -28,22 +24,39 @@ n_ens_members = 40
 member_start = 1
 member_end = 40
 # Start date, end date, and interval for history files to process
-start
+start_year = 2016
+start_month = 3
+start_day = 31
+start_hour = 18
+start_min = 0
+start_sec = 0
+start_date = datetime(start_year, start_month, start_day, start_hour, start_min, start_sec)
 
-# Start, End, Step time for history files to process
-start_time = 36000.
-end_time = 54000.
-step_time = 300.
+end_year = 2016
+end_month = 4
+end_day = 1
+end_hour = 2
+end_min = 45
+end_sec = 0
+end_date = datetime(end_year, end_month, end_day, end_hour, end_min, end_sec)
+
+history_interval = 900  # seconds
+
+datetime_range = [start_date + timedelta(seconds=x) for x in
+                  range(0, (end_date-start_date).seconds + history_interval, history_interval)]
+
 # Directory to save interpolated history files
-outputdir = "/Volumes/depot/dawson29/data/VORTEXSE/simulations/ARPS/2016_IOP3/EnKF/3km153x153_6kmconvicbc"
-# Option to run arpsintrp on the generated files
-run_arpsintrp = True
-run_t0 = False
-run_lbc = True
-# Absolute path to arpsintrp executable
-arpsintrp_exe = '/Users/dawson29/arps5.4_exp/bin/arpsintrp_mpi'
+outputdir = "/depot/dawson29/data/VORTEXSE/simulations/ARPS/2016_IOP3/EnKF/3km153x153_newseicbc"
+# Option to run wrf2arps on the generated files
+run_wrf2arps = False
+run_t0 = True
+run_lbc = False
+# Absolute path to wrf2arps executable
+wrf2arps_exe = '/home/dawson29/arps5.4_main/bin/wrf2arps'
 # MPI executable (if desired)
-mpi_exe = '/opt/openmpi/bin/mpirun'
+run_mpi = False
+mpi_exe = 'mpiexec'
+mpi_numproc_arg = '-n'
 # MPI parameters
 nproc_x = 5
 nproc_y = 2
@@ -51,89 +64,72 @@ nproc_x_in = 10
 nproc_y_in = 6
 nproc_x_out = 1
 nproc_y_out = 1
-inisplited = 0000001
-dmp_out_joined = 1111111
+# inisplited = 0000001
+dmp_out_joined = 1  # 1111111
 
 ### END OF USER SPECIFICATIONS ###
 
-import os
-import glob
-import sys
-import subprocess
-sys.path.append('/Users/dawson29/arpsEnKFtools/modules/')
-from editNamelist import editNamelistFile
-
-ens_member_list = xrange(member_start, member_end + 1)
+ens_member_list = range(member_start, member_end + 1)
 ens_member_names = ["ena%03d" % m for m in ens_member_list]
-t0_interp_input_file_names = ["%s/%s.arpsintrp_t0.input" % (rinPath, ens_member_name)
-                           for ens_member_name in ens_member_names]
-t0_interp_output_file_names = ["%s/%s.arpsintrp_t0.output" % (rinPath, ens_member_name)
-                            for ens_member_name in ens_member_names]
-lbc_interp_input_file_names = ["%s/%s.arpsintrp_lbc.input" % (rinPath, ens_member_name)
-                           for ens_member_name in ens_member_names]
-lbc_interp_output_file_names = ["%s/%s.arpsintrp_lbc.output" % (rinPath, ens_member_name)
-                            for ens_member_name in ens_member_names]
-member_paths = ["%s/EN%03d/%s" % (basedir, m, ens_member_name) for m, ens_member_name in
+t0_interp_input_file_names = ["%s/%s.wrf2arps_t0.input" % (rinPath, ens_member_name)
+                              for ens_member_name in ens_member_names]
+t0_interp_output_file_names = ["%s/%s.wrf2arps_t0.output" % (rinPath, ens_member_name)
+                               for ens_member_name in ens_member_names]
+lbc_interp_input_file_names = ["%s/%s.wrf2arps_lbc.input" % (rinPath, ens_member_name)
+                               for ens_member_name in ens_member_names]
+lbc_interp_output_file_names = ["%s/%s.wrf2arps_lbc.output" % (rinPath, ens_member_name)
+                                for ens_member_name in ens_member_names]
+
+newse_timestrings = [d.strftime('%Y-%m-%d_%H:%M:%S') for d in datetime_range]
+member_paths = ["{}/%03d/%s" % (basedir, m, ens_member_name) for m, ens_member_name in
                 zip(ens_member_list, ens_member_names)]
 
-for ens_member_name, t0_interp_input_file_name, lbc_interp_input_file_name, member_path in \
-        zip(ens_member_names, t0_interp_input_file_names, lbc_interp_input_file_names, \
-        member_paths):
+for ens_member_name, t0_interp_input_file_name, lbc_interp_input_file_name, newse_timestring in \
+        zip(ens_member_names, t0_interp_input_file_names, lbc_interp_input_file_names,
+            newse_timestrings):
 
     # Initial conditions
-    editNamelistFile("%s" % template, t0_interp_input_file_name,
-        runname="%s" % ens_member_name,
-        hdmpfheader=member_path,
-        dirname=outputdir,
-        tbgn_dmpin=start_time,
-        tend_dmpin=start_time,
-        tintv_dmpin=step_time,
-        nproc_x=nproc_x,
-        nproc_y=nproc_y,
-        nproc_x_in=nproc_x_in,
-        nproc_y_in=nproc_y_in,
-        nproc_x_out=nproc_x_out,
-        nproc_y_out=nproc_y_out,
-        inisplited=inisplited,
-        dmp_out_joined=dmp_out_joined)
+    editNamelistFile("{}".format(template), t0_interp_input_file_name,
+                     dir_extd=basedir,
+                     runname="%s" % ens_member_name,
+                     init_time_str=newse_timestring,
+                     start_time_str=newse_timestring,
+                     end_time_str=newse_timestring,
+                     dirname=outputdir,
+                     dmp_out_joined=dmp_out_joined,
+                     hdmpfmt=3,
+                     exbcdmp=0)
 
     # Boundary conditions
-    editNamelistFile("%s" % template, lbc_interp_input_file_name,
-        runname="%s" % ens_member_name,
-        hdmpfheader=member_path,
-        dirname=outputdir,
-        tbgn_dmpin=start_time,
-        tend_dmpin=end_time,
-        tintv_dmpin=step_time,
-        nproc_x=nproc_x,
-        nproc_y=nproc_y,
-        nproc_x_in=nproc_x_in,
-        nproc_y_in=nproc_y_in,
-        nproc_x_out=nproc_x_out,
-        nproc_y_out=nproc_y_out,
-        inisplited=inisplited,
-        dmp_out_joined=dmp_out_joined,
-        hdmpfmt=0,
-        exbcdmp=3)
+    editNamelistFile("{}".format(template), t0_interp_input_file_name,
+                     dir_extd=basedir,
+                     runname="%s" % ens_member_name,
+                     init_time_str=newse_timestring,
+                     start_time_str=newse_timestring,
+                     end_time_str=newse_timestring,
+                     dirname=outputdir,
+                     dmp_out_joined=dmp_out_joined,
+                     hdmpfmt=0,
+                     exbcdmp=3)
 
-if run_arpsintrp:
+if run_wrf2arps:
     for t0_interp_input_file_name, t0_interp_output_file_name, lbc_interp_input_file_name, \
-            lbc_interp_output_file_name in zip(t0_interp_input_file_names, \
-            t0_interp_output_file_names, lbc_interp_input_file_names, \
-            lbc_interp_output_file_names):
-
-        command='%s -np %d %s' % (mpi_exe, nproc_x*nproc_y, arpsintrp_exe)
+            lbc_interp_output_file_name in zip(t0_interp_input_file_names,
+                                               t0_interp_output_file_names,
+                                               lbc_interp_input_file_names,
+                                               lbc_interp_output_file_names):
+        if run_mpi:
+            command = '{} {} {:d} {}'.format(mpi_exe, mpi_numproc_arg, nproc_x*nproc_y,
+                                             wrf2arps_exe)
+        else:
+            command = '{}'.format(wrf2arps_exe)
         if run_t0:
             with open(t0_interp_input_file_name, 'r') as input, \
                  open(t0_interp_output_file_name, 'w') as output:
-                print "Running %s for %s" % (arpsintrp_exe, t0_interp_input_file_name)
+                print("Running %s for %s" % (wrf2arps_exe, t0_interp_input_file_name))
                 subprocess.call(command, stdin=input, stdout=output, shell=True)
         if run_lbc:
             with open(lbc_interp_input_file_name, 'r') as input, \
                  open(lbc_interp_output_file_name, 'w') as output:
-                print "Running %s for %s" % (arpsintrp_exe, lbc_interp_input_file_name)
+                print("Running %s for %s" % (wrf2arps_exe, lbc_interp_input_file_name))
                 subprocess.call(command, stdin=input, stdout=output, shell=True)
-
-
-
-
