e13e35dfe48f8ccce32739292088169020ebb36a
Dan Dawson <dandawson@purdue.edu>
2019-03-12 13:09:43 -0400

    WIP

--- template/setup_experiment.sh
+++ template/setup_experiment.sh
@@ -5,7 +5,7 @@ SCRATCHBASE="/scratch/rice/d/dawson29/"
 DEPOTBASE="/depot/dawson29/"
 BASEDIR="${SCRATCHBASE}VORTEXSE/simulations/ARPS/2016_IOP3/EnKF/"
 RUNNAME="3km153x153_newse"
-TEMPLATEDIR="${DEPOTBASE}apps/arpsEnKFtools/EnKF_template/"
+TEMPLATEDIR="${DEPOTBASE}apps/arpsEnKFtools/template/"
 SFCDATA="${DEPOTBASE}data/VORTEXSE/simulations/ARPS/sfcdata/3km153x153.sfcdata"
 TRNDATA="${DEPOTBASE}data/VORTEXSE/simulations/ARPS/trndata/3km153x153.trndata"
 BOUNDARYDIR="${DEPOTBASE}data/VORTEXSE/simulations/ARPS/2016_IOP3/EnKF/3km153x153_newseicbc/"
@@ -14,6 +14,7 @@ SCATTDIR="/home/dawson29/arps5.4_main/data/scatt/"
 ARPSEXE="/home/dawson29/arps5.4_main/bin/arps_mpi"
 ARPSENKFICEXE="/home/dawson29/arps5.4_main/bin/arpsenkfic"
 ARPSENKFEXE="/home/dawson29/arps5.4_main/bin/arpsenkf_mpi"
+RADFLAG="2016_IOP3.radflag"
 PERTURBIC=0
 INIFILE="3km033116NAM107x107bgandbc.hdf010800"
 INIBASE="3km033116NAM107x107bgandbc.hdfgrdbas"
@@ -40,7 +41,7 @@ ln -sf ${RADARDIR} nexrad
 ln -sf ${SCATTDIR} scatt
 cp ${TEMPLATEDIR}/run_real_data_case.py .
 cp ${TEMPLATEDIR}/run_real_data_case.csh .
-cp ${TEMPLATEDIR}/template.radflag .
+cp ${TEMPLATEDIR}/${RADFLAG} .
 mkdir -p inputfiletemplates
 cp ${TEMPLATEDIR}/arps.input inputfiletemplates/
 cp ${TEMPLATEDIR}/arpsenkfic.input inputfiletemplates/
