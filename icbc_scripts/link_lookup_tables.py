083eac4054b3669c564b58c9d176fb8ed8187eb0
Dan Dawson <dandawson@purdue.edu>
2019-04-22 16:52:43 -0400

    Bugfixes to link_lookup_tables.py

--- icbc_scripts/link_lookup_tables.py
+++ icbc_scripts/link_lookup_tables.py
@@ -4,7 +4,7 @@
 import os
 
 # Absolute path to directory where lookup tables reside
-basedir = "/depot/dawson29/data/Projects/VORTEXSE/simulations/ARPS/2016_IOP3/scatt_1km453x453/S-band/"
+basedir = "/depot/dawson29/data/Projects/VORTEXSE/simulations/ARPS/2016_IOP3/EnKF/scatt_1km453x453/S-band/"
 category_names = ['RAIN', 'RIME']
 moment_names = ['Zh', 'Zhv', 'Zv', 'k']
 density_list = range(100, 1000, 100)
@@ -15,7 +15,7 @@ nproc_y = 6
 target_filenames_by_category = []
 for category in category_names:
     if category in 'RIME':
-        filenames = ["{}_rho{:4d}.hdf{}000000".format(category, density, moment_name)
+        filenames = ["{}_rho{:04d}.hdf{}000000".format(category, density, moment_name)
                      for density in density_list for moment_name in moment_names]
     else:
         filenames = ["{}.hdf{}000000".format(category, moment_name)
@@ -25,10 +25,10 @@ for category in category_names:
 # Generate the link names to the targets and make the links
 for target_filenames in target_filenames_by_category:
     for target_filename in target_filenames:
-        file_link_names = ["{}_{:3d}{:3d}".format(target_filename, proc_x, proc_y)
-                           for proc_x in range(nproc_x) for proc_y in range(nproc_y)]
+        file_link_names = ["{}_{:03d}{:03d}".format(target_filename, proc_x, proc_y)
+                           for proc_x in range(1, nproc_x + 1) for proc_y in range(1, nproc_y + 1)]
         for file_link in file_link_names:
             cmd = "ln -sf {} {}".format(os.path.join(basedir, target_filename),
                                         os.path.join(basedir, file_link))
-        print(cmd)
-        # os.system(cmd)
+            print(cmd)
+            os.system(cmd)
