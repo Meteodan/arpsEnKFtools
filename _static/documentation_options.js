04d70448b40aea66bed9b4aa12877b2b525162e3
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-08 22:36:39 +0000

    Update docs after building Travis build 48 of Meteodan/arpsEnKFtools
    The docs were built from the branch 'master' against the commit
    b048b3be58f3a68b0c5509d470542337de471ad5.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/Meteodan/arpsEnKFtools/jobs/273683166.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html/ .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post101+g90b3781',
+    VERSION: '0.post105+gb048b3b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
